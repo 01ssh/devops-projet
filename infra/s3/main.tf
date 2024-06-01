b54d779ad38d49b0381e21e3666b32f250f2a034
Ubuntu <ubuntu@ip-172-31-25-159.eu-west-1.compute.internal>
2024-06-01 12:35:56 +0000

    modif

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,5 +1,5 @@
 # VARIABLES
-/*variable "bucket_name" {}
+variable "bucket_name" {}
 variable "name" {}
 variable "environment" {}
 
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "remote_state_bucket" {
     Name        = var.name
     Environment = var.environment
   }
-}*/
+}
 
 
 a#ws s3api create-bucket --bucket remote-projet-rncp --region eu-west-3 --create-bucket-configuration LocationConstraint=eu-west-3      

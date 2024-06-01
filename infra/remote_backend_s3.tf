7bb2f4ea6ee7bbe78d5aea55db44fad8d8bb3d44
Ubuntu <ubuntu@ip-172-31-25-159.eu-west-1.compute.internal>
2024-06-01 12:51:26 +0000

    modif

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-9999999999"
+    bucket = "dev-proj-1-remote-state-bucket-9999999"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-west-3"
   }

333f61f1b53731a7fea851e566597006bb221961
Ubuntu <ubuntu@ip-172-31-3-246.eu-west-1.compute.internal>
2024-06-04 22:34:21 +0000

    script

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
-# shellcheck disable=SC2164
-cd /home/ubuntu
+set -e
+echo "Updating packages"
 yes | sudo apt update
+echo "Installing python3 and pip3"
 yes | sudo apt install python3 python3-pip
+echo "Cloning the repository"
 git clone https://github.com/prkltos/python-mysql-db-proj-1.git
 sleep 20
-# shellcheck disable=SC2164
-sudo apt-get install nodejs npm
+echo "Installing nodejs and npm"
+yes |  sudo apt-get install nodejs npm
 nodejs -v
 npm -v
+echo "Installing python3-venv"
+sudo apt-get install python3-venv
+echo "Navigating to the project directory"
 cd python-mysql-db-proj-1
+echo "Creating a virtual environment"
+python3 -m venv venv
+echo "Activating the virtual environment"
+source venv/bin/activate
+echo "Installing requirements"
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &

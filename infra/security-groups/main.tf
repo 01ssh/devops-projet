10324fe42b53663e636c56f6a26a86ce8e3b0eac
Ubuntu <ubuntu@ip-172-31-3-246.eu-west-1.compute.internal>
2024-06-13 09:30:20 +0000

    prometheus

--- infra/security-groups/main.tf
+++ infra/security-groups/main.tf
@@ -104,13 +104,3 @@ resource "aws_security_group_rule" "allow_grafana_port" {
   security_group_id = aws_security_group.ec2_sg_python_api.id
 }
 
-
-/*resource "aws_security_group_rule" "allow_prometheus_port" {
-  type              = "ingress"
-  from_port         = 9090
-  to_port           = 9090
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_sg_python_api.id
-}
-*/

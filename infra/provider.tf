d23b61155a5a7ed8ce29c943d47e804e7a06c465
Ubuntu <ubuntu@ip-172-31-3-246.eu-west-1.compute.internal>
2024-06-13 22:36:11 +0000

    final

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,3 +3,15 @@ provider "aws" {
   shared_credentials_files = ["/Ubuntu/.aws/credentials"]
 }
 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}

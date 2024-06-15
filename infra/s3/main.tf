e2b6ed4e4ccb36cee2edc62db0ad50f6f52ddd2f
Ubuntu <ubuntu@ip-172-31-3-246.eu-west-1.compute.internal>
2024-06-15 21:19:07 +0000

    test

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,13 +1,13 @@
 # VARIABLES
-variable "bucket_name" {}
-variable "name" {}
-variable "environment" {}
+#variable "bucket_name" {}
+#variable "name" {}
+#variable "environment" {}
 
-output "remote_state_s3_bucket_name" {
-  value = aws_s3_bucket.remote_state_bucket.id
-}
+#output "remote_state_s3_bucket_name" {
+#  value = aws_s3_bucket.remote_state_bucket.id
+#}
 
-resource "aws_s3_bucket" "remote_state_bucket" {
+/*resource "aws_s3_bucket" "remote_state_bucket" {
   bucket = var.bucket_name
 
    lifecycle {
@@ -19,6 +19,6 @@ resource "aws_s3_bucket" "remote_state_bucket" {
     Environment = var.environment
   }
 }
-
+*/
 
 #aws s3api create-bucket --bucket remote-projet-rncp --region eu-west-3 --create-bucket-configuration LocationConstraint=eu-west-3      
